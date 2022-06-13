f1d488db49fc6b5dbb769fc21511a93ef7fc55d9
Dmitriy Chernyshov <dechernyshov@bars.group>
2022-06-13 19:27:00 +0300

    Реализация заданий блока 1

--- block_1/memory_usage/task_1/implementation.py
+++ block_1/memory_usage/task_1/implementation.py
@@ -1,5 +1,15 @@
+import os
+import weakref
+
+
 class TempFile:
 
     def __init__(self, path) -> None:
         super().__init__()
         self._f = open(path, 'wb')
+        weakref.finalize(self, self._delete, self._f)
+
+    @staticmethod
+    def _delete(file):
+        file.close()
+        os.remove(file.name)
--- block_1/threads_and_processes/task_1/implementation.py
+++ block_1/threads_and_processes/task_1/implementation.py
@@ -1,2 +1,65 @@
+import time
+import uuid
+from multiprocessing import Process, Queue
+
+
+class Front:
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._worker = None
+        self._messages = Queue()
+
+    def get_result(self, id_):
+        pass
+
+    def call_command(self, command_name, params):
+        id_ = uuid.uuid4()
+        self._messages.put((id_, command_name, params))
+
+        return id_
+
+    def start(self):
+        self._worker = Process(target=self._run, args=(self._messages,))
+        self._worker.start()
+
+    def stop(self):
+        self._worker.terminate()
+        self._worker.join()
+
+    @staticmethod
+    def _run(messages):
+        while True:
+            message = messages.get()
+            if message:
+                pass
+
+            time.sleep(1.0)
+
+
+class Back:
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+
 class Composer:
-    pass
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._front = Front()
+        self._back = Back()
+
+    def start(self):
+        self._back.start()
+        self._front.start()
+
+    def stop(self):
+        self._front.stop()
+        self._back.stop()
+
+    def get_front(self):
+        return self._front
